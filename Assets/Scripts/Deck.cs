6c4c74329640a09743d31bd94073a561a95f633b
Wayne Livingston <wayneplivingston@gmail.com>
2025-04-09 18:24:04 -0400

    Finally cards can be played, discarded, dealt, and repeat

--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -13,7 +13,10 @@ public class Deck : MonoBehaviour
     {
         foreach (Card card in playerDeck)
         {
-            cardsInDeck.Add(card);
+            GameObject cardObject = Instantiate(card.gameObject);
+
+            cardsInDeck.Add(cardObject.GetComponent<Card>());
+            cardObject.SetActive(false);
         }
         for (int i = 0; i < 5; i++)
         {
@@ -30,24 +33,16 @@ public class Deck : MonoBehaviour
 
     public void DealCard(int handPosition)
     {
-        if (cardsInDeck.Count == 0)
-        {
-            cardsInDeck = cardsInDiscard;
-            cardsInDiscard = new List<Card>();
-            foreach (Card newCard in cardsInDeck)
-            {
-                newCard.SetLocation(Location.Deck);
-            }
-        }
+
         Card card = cardsInDeck[Random.Range(0, cardsInDeck.Count)];
         cardsInField.Add(card);
         cardsInDeck.Remove(card);
         card.SetLocation(Location.Field);
-        GameObject cardObject = Instantiate(card.gameObject);
 
-        cardObject.GetComponent<Card>().handPosition = handPosition;
+        card.handPosition = handPosition;
         int cardPosition = (handPosition * -8) + 8;
-        cardObject.transform.position = new Vector3(cardPosition, -17, -10);
+        card.gameObject.transform.position = new Vector3(cardPosition, -17, -10);
+        card.gameObject.SetActive(true);
     }
 
     public void DiscardCard(Card discardedCard)
@@ -57,5 +52,14 @@ public class Deck : MonoBehaviour
         cardsInDiscard.Add(discardedCard);
         DealCard(discardedCard.handPosition);
         discardedCard.gameObject.SetActive(false);
+        if (cardsInDeck.Count == 0)
+        {
+            cardsInDeck = cardsInDiscard;
+            cardsInDiscard = new List<Card>();
+            foreach (Card newCard in cardsInDeck)
+            {
+                newCard.SetLocation(Location.Deck);
+            }
+        }
     }
 }
